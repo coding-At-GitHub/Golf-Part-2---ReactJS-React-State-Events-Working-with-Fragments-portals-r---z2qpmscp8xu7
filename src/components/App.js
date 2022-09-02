d5026b3997e504c7ef3102fb8dcd56cb8b7db07a
Suresh Dukiya <rjsh1503@gmail.com>
2022-09-02 17:54:00 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,62 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
+  const reset = () => {
+    setRenderBall(!renderBall);
+    setBallPosition({ left: "0px", top: "0px" });
+  };
   const renderChoice = () => {};
+  const start = () => {
+    setRenderBall(!renderBall);
+  };
+  const keyHandler = (event) => {
+    let ballPositionClone = { ...ballPosition };
+
+    if (event.keyCode === 37) {
+      ballPositionClone.left =
+        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) -
+        5 +
+        "px";
+    } else if (event.keyCode === 39) {
+      ballPositionClone.left =
+        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) +
+        5 +
+        "px";
+    } else if (event.keyCode === 38) {
+      ballPositionClone.top =
+        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) -
+        5 +
+        "px";
+    } else if (event.keyCode === 40) {
+      ballPositionClone.top =
+        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) +
+        5 +
+        "px";
+    }
+    // ballPositionClone.left = (Number(ballPosition.left.slice(0,ballPosition.left.indexOf("p")))+5)+"px";
+    setBallPosition(ballPositionClone);
+    // console.log(ballPosition);
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", keyHandler);
+    return () => window.removeEventListener("keydown", keyHandler);
+  });
+
+  let compo;
+  if (renderBall) {
+    compo = (
+      <div
+        className="ball"
+        style={{ left: ballPosition.left, top: ballPosition.top }}
+      ></div>
+    );
+  } else {
+    compo = (
+      <button onClick={start} className="start">
+        Start
+      </button>
+    );
+  }
 
   return (
     <div className="playground">
